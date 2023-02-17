1e545d2efecc6a71ca1bc200a81a1c9f677de3fa
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2023-02-17 04:01:49 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,32 +2,33 @@ library(shiny)
 
 ui <- fluidPage(
   br(),
-  dateInput(
-    inputId = "date",
-    label = span(icon("calendar"), "Date"),
-    # Current Date as initial value
-    value = Sys.Date(),
-    format = "yyyy-mm-dd"
+  wellPanel(
+    fluidRow(
+      column(12, dateInput(
+        inputId = "date",
+        label = span(icon("calendar"), "Date"),
+        # Current Date as initial value
+        value = Sys.Date(),
+        format = "yyyy-mm-dd"
+      )),
+      # Use columns to get hour and minutes in same row
+      column(6, selectInput(
+        inputId = "hour",
+        label = span(icon("clock"), "Hour"),
+        choices = gsub(pattern = " ", replacement = "0", x = format(seq(0, 23), width = 2)),
+        # Current Time as initial value
+        selected = format(Sys.time(), "%H")
+      )),
+      # Use columns to get hour and minutes in same row
+      column(6, selectInput(
+        inputId = "min",
+        label = "Minutes",
+        choices = gsub(pattern = " ", replacement = "0", x = format(seq(0, 59), width = 2)),
+        # Current Time as initial value
+        selected = format(Sys.time(), "%M")
+      ))
+    )
   ),
-  br(),
-  # Use columns to get hour and minutes in same row
-  column(4, selectInput(
-    inputId = "hour",
-    label = span(icon("clock"), "Hour"),
-    choices = gsub(pattern = " ", replacement = "0", x = format(seq(0, 23), width = 2)),
-    # Current Time as initial value
-    selected = format(Sys.time(), "%H")
-  )),
-  # Use columns to get hour and minutes in same row
-  column(4, selectInput(
-    inputId = "min",
-    label = "Minutes",
-    choices = gsub(pattern = " ", replacement = "0", x = format(seq(0, 59), width = 2)),
-    # Current Time as initial value
-    selected = format(Sys.time(), "%M")
-  )),
-  br(),
-  strong("Output Date and Time"),
   br(),
   verbatimTextOutput("dateTimeOutput")
 )
